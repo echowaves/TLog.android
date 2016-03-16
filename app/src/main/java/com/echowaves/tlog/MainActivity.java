46f0700afcc47d46005e8bc08e31eb39c29dafa5
dmitryame <dmitry@custommode.com>
2016-03-16 15:51:43 -0400

    localytics

--- app/src/main/java/com/echowaves/tlog/MainActivity.java
+++ app/src/main/java/com/echowaves/tlog/MainActivity.java
@@ -1,13 +1,15 @@
 package com.echowaves.tlog;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
