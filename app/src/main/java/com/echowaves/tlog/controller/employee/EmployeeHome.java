c267c8f0d6d6d757012fef67830d56e4618dd4dc
dmitryame <dmitry@custommode.com>
2016-04-14 20:54:10 -0400

    employee home making progress

--- app/src/main/java/com/echowaves/tlog/controller/employee/EmployeeHome.java
+++ app/src/main/java/com/echowaves/tlog/controller/employee/EmployeeHome.java
@@ -8,19 +8,18 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import com.echowaves.tlog.R;
 import com.echowaves.tlog.TLApplicationContextProvider;
 import com.echowaves.tlog.controller.user.SignIn;
-import com.echowaves.tlog.controller.user.employee.EmployeeDetails;
 import com.echowaves.tlog.model.TLActionCode;
 import com.echowaves.tlog.model.TLCheckin;
 import com.echowaves.tlog.model.TLEmployee;
 import com.echowaves.tlog.util.TLJsonHttpResponseHandler;
+
+import net.danlew.android.joda.JodaTimeAndroid;
 
 import org.joda.time.DateTime;
 import org.json.JSONArray;
@@ -32,10 +31,12 @@ import java.util.Date;
 
 import cz.msebera.android.httpclient.Header;
 
-public class    EmployeeHome extends AppCompatActivity {
+public class EmployeeHome extends AppCompatActivity {
     private Context context;
 
     private Button signoutButton;
+    private TextView title;
+
 
     private TLEmployee currentEmployee;
     private ArrayList<TLCheckin> currentCheckins;
@@ -46,6 +47,7 @@ public class    EmployeeHome extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.employee_activity_employee_home);
         context = this;
+        JodaTimeAndroid.init(this);
 
         signoutButton = (Button) findViewById(R.id.employee_activity_employee_home_signOutButton);
         signoutButton.setOnClickListener(new View.OnClickListener() {
@@ -61,7 +63,7 @@ public class    EmployeeHome extends AppCompatActivity {
 
     void loadCheckins() {
 
-        TLCheckin.getAllCheckins(0, 100 , new TLJsonHttpResponseHandler(context) {
+        TLCheckin.getAllCheckins(0, 100, new TLJsonHttpResponseHandler(context) {
                     @Override
                     public void onSuccess(int statusCode, Header[] headers, JSONObject jsonResponse) {
                         try {
@@ -73,44 +75,42 @@ public class    EmployeeHome extends AppCompatActivity {
                                             jsonEmployee.getString("name"),
                                             jsonEmployee.getString("email"),
                                             jsonEmployee.getBoolean("is_subcontractor")
-                            );
+                                    );
 
 
-
-
-                            JSONArray jsonCheckins = (JSONArray)jsonResponse.get("checkins");
+                            JSONArray jsonCheckins = (JSONArray) jsonResponse.get("checkins");
 
                             ArrayList<TLCheckin> checkins = new ArrayList<TLCheckin>();
 
-                            for (int i=0; i<jsonCheckins.length(); i++) {
+                            for (int i = 0; i < jsonCheckins.length(); i++) {
                                 JSONObject jsonCheckin = jsonCheckins.getJSONObject(i);
 
 
                                 TLActionCode actionCode =
                                         new TLActionCode(
                                                 jsonCheckin.getInt("action_code_id"),
-                                        jsonCheckin.getString("code"),
-                                        jsonCheckin.getString("description"));
+                                                jsonCheckin.getString("code"),
+                                                jsonCheckin.getString("description"));
 
 
 //                                2015-12-22 22:34:51.326
                                 Date checkinDate = new DateTime(jsonCheckin.getString("checked_in_at")).toDate();
 
-                                        Integer interval =
-                                                jsonCheckin.getJSONObject("duration").getInt("hours") * 60 * 60 +
-                                                jsonCheckin.getJSONObject("duration").getInt("minutes") * 60 +
-                                                        jsonCheckin.getJSONObject("duration").getInt("seconds");
 
+                                Integer interval =
+                                        jsonCheckin.getJSONObject("duration").optInt("hours", 0) * 60 * 60
+                                                + jsonCheckin.getJSONObject("duration").optInt("minutes", 0) * 60
+                                                + jsonCheckin.getJSONObject("duration").optInt("seconds", 0);
 
 
                                 TLCheckin checkin =
                                         new TLCheckin(
                                                 jsonCheckin.getInt("id"),
-                                        jsonCheckin.getString("email"),
-                                        jsonCheckin.getInt("user_id"),
-                                        checkinDate,
-                                        interval,
-                                        actionCode);
+                                                jsonCheckin.getString("email"),
+                                                jsonCheckin.getInt("user_id"),
+                                                checkinDate,
+                                                interval,
+                                                actionCode);
 
 
                                 checkins.add(checkin);
@@ -121,9 +121,9 @@ public class    EmployeeHome extends AppCompatActivity {
                             currentCheckin = null;
 
                             //iterate over checkins and find a current checkin that is active (duration == 0)
-                            for(int index = 0; index < checkins.size(); index++) {
-                                if(checkins.get(index).getDuration() == 0) {
-                                    if( currentCheckin == null) {
+                            for (int index = 0; index < checkins.size(); index++) {
+                                if (checkins.get(index).getDuration() == 0) {
+                                    if (currentCheckin == null) {
                                         currentCheckin = checkins.get(index);
                                         currentCheckins.remove(index);
                                     }
@@ -157,6 +157,9 @@ public class    EmployeeHome extends AppCompatActivity {
     }
 
     void updateViews() {
+        title = (TextView) findViewById(R.id.employee_activity_employee_home_title);
+        title.setText(currentEmployee.getName());
+
 //        navBar.topItem?.title = employee.name
 //        if (self.currentCheckin != nil) {
 //            self.checkinButton!.setTitle("Check Out", forState: .Normal)
@@ -177,8 +180,6 @@ public class    EmployeeHome extends AppCompatActivity {
 //        })
 //        //        self.tableView.reloadInputViews()
     }
-
-
 
 
     @Override
