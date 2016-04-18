538a58482fdf5b264a86db8e0bf46eec3e73657f
dmitryame <dmitry@custommode.com>
2016-04-18 14:38:26 -0400

    reports progress

--- app/src/main/java/com/echowaves/tlog/controller/user/reports/YearPicker.java
+++ app/src/main/java/com/echowaves/tlog/controller/user/reports/YearPicker.java
@@ -2,15 +2,33 @@ package com.echowaves.tlog.controller.user.reports;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 import com.echowaves.tlog.R;
 
 public class YearPicker extends AppCompatActivity {
 
+    private Button backButton;
+
+    private ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_reports_activity_year_picker);
+
+        backButton = (Button) findViewById(R.id.user_reports_activity_year_picker_backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(final View v) {
+                onBackPressed();
+            }
+        });
+
+
+        listView = (ListView) findViewById(R.id.user_reports_activity_year_picker_listView);
+
     }
 
     @Override
