55c17c285c13dc53eb2a4e834b3dcf7bbb3acef7
dmitryame <dmitry@custommode.com>
2016-05-03 10:13:19 -0400

    wip:adding datetime pickers

--- app/src/main/java/com/echowaves/tlog/controller/employee/CheckinDetails.java
+++ app/src/main/java/com/echowaves/tlog/controller/employee/CheckinDetails.java
@@ -1,13 +1,17 @@
 package com.echowaves.tlog.controller.employee;
 
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
 
 import com.echowaves.tlog.R;
 import com.echowaves.tlog.TLApplicationContextProvider;
@@ -19,9 +23,15 @@ import com.localytics.android.Localytics;
 import org.joda.time.DateTime;
 import org.json.JSONObject;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 import cz.msebera.android.httpclient.Header;
 
 public class CheckinDetails extends AppCompatActivity {
+    private Context context;
 
     private Button backButton;
     private Button deleteButton;
@@ -37,6 +47,8 @@ public class CheckinDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Localytics.tagEvent("CheckinDetails");
+
+        context = this;
 
         setContentView(R.layout.employee_activity_checkin_details);
 
@@ -114,11 +126,51 @@ public class CheckinDetails extends AppCompatActivity {
         checkInAtText = (EditText) findViewById(R.id.employee_activity_checkin_details_checkInAtText);
         checkInAtText.setText(new DateTime(checkin.getCheckedInAt()).toString(TLConstants.defaultDateFormat));
 
+        checkInAtText.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                    final View dialogView = View.inflate(context, R.layout.dialog_date_time_picker, null);
+                    final AlertDialog alertDialog = new AlertDialog.Builder(context).create();
+                    dialogView.findViewById(R.id.dialog_date_time_picker_setbutton).setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            DatePicker datePicker = (DatePicker) dialogView.findViewById(R.id.dialog_date_time_picker_date);
+                            TimePicker timePicker = (TimePicker) dialogView.findViewById(R.id.dialog_date_time_picker_time);
+                            Calendar calendar = new GregorianCalendar(datePicker.getYear(),
+                                    datePicker.getMonth(),
+                                    datePicker.getDayOfMonth(),
+                                    timePicker.getCurrentHour(),
+                                    timePicker.getCurrentMinute());
+                            SimpleDateFormat mSDF = new SimpleDateFormat("HH:mm:ss");
+                            String time = mSDF.format(calendar.getTime());
+                            int day = datePicker.getDayOfMonth();
+                            int month = datePicker.getMonth();
+                            int year = datePicker.getYear();
+                            SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy");
+                            String formatedDate = sdf.format(new Date(year-1900, month, day));
+//                            editText_datetime.setText(formatedDate + ' ' + time);
+                            alertDialog.dismiss();
+                        }
+                    });
+                    alertDialog.setView(dialogView);
+                    alertDialog.show();
+
+
+            }
+        });
+
+
+
+
         durationText = (EditText) findViewById(R.id.employee_activity_checkin_details_durationText);
         durationText.setText(checkin.getDurationExtendedText());
 
         actionCodeText = (EditText) findViewById(R.id.employee_activity_checkin_details_actionCodeText);
         actionCodeText.setText(checkin.getActionCode().getCode() + ":" + checkin.getActionCode().getDescr());
+
 
 
     }
